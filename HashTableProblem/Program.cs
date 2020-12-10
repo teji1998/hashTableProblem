a443714fd68d48c11b6b7d0f15ea86765e9ca7d7
“teji1998” <tejasvinirpk@gmail.com>
2020-12-10 09:21:10 +0530

    Added :Code to find frequency of words in paragraph

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -8,16 +8,26 @@ namespace HashTableProblem
         {
             Console.WriteLine("Welcome to the hash table problem !! ");
             MapNode<int, string> hash = new MapNode<int, string>(6);
-            
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
+
+            /*   hash.Add(0, "To");
+               hash.Add(1, "be");
+               hash.Add(2, "or");
+               hash.Add(3, "not");
+               hash.Add(4, "to");
+               hash.Add(5, "be");*/
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] breakParagraph = paragraph.Split(" ");
+            int key = 0;
+            //Loop iterates on paragraph and adds key and value of each substing
+            //To hash
+            foreach (string word in breakParagraph)
+            {
+                hash.Add(key, word);
+                key ++;
+            }
             Frequency freq = new Frequency();
 
             freq.WordFrequency(hash);
-                }
+         }
     }
 }
