3ab306f1a3039b1573642035375dd2734e9ccd95
“teji1998” <tejasvinirpk@gmail.com>
2020-12-10 09:02:51 +0530

    Added:Code to find the frequency of words

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -7,6 +7,17 @@ namespace HashTableProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the hash table problem !! ");
-        }
+            MapNode<int, string> hash = new MapNode<int, string>(6);
+            
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Frequency freq = new Frequency();
+
+            freq.WordFrequency(hash);
+                }
     }
 }
